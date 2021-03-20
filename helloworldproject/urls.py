1536165258b46cdb17024c8e0526bdaf15894bb0
noromap <noromap.ai@gmail.com>
2021-03-20 10:16:16 +0000

    first coment

---
+++ helloworldproject/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path
+from .views import helloworldfunc
+from .views import HelloWorldClass
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('helloworldurl/', helloworldfunc),
+    path('helloworldurl2/', HelloWorldClass.as_view()),
+]
---
+++ helloworldproject/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+from django.views.generic import TemplateView
+
+
+def helloworldfunc(request):
+    return HttpResponse('<h1>Hello World!</h1>')
+
+
+class HelloWorldClass(TemplateView):
+    template_name = "hello.html"
